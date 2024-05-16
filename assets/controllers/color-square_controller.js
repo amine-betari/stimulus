92d6817bed3c8cf396fdc4d2f061a7c327c10980
BETARI Amine <a.betari@inforca.mc>
2024-05-16 14:57:31 +0200

    WIP : color-square

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,16 +1,42 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class  extends Controller {
-    static  targets = ['colorSquare', 'select'];
+
+    static targets = ['colorSquare', 'select'];
+    static values = {
+        colorId: Number
+    }
 
     connect() {
-        this.selectTarget.classList.add('d-none');
+     //   this.selectTarget.classList.add('d-none');
+        // Concept of Api Values
+        console.log(this.colorIdValue);
+
+        // See colorIdValueChanged()
+        /*if(this.colorIdValue) {
+            this.setSelectedColor(this.colorIdValue);
+        }*/
+    }
+
+    colorIdValueChanged(){
+        // this.setSelectedColor(this.colorIdValue);
+        this.selectTarget.value = this.colorIdValue;
+
+        this.colorSquareTargets.forEach((element) => {
+            if (element.dataset.colorId == this.colorIdValue) {
+                element.classList.add('selected');
+            } else {
+                element.classList.remove('selected');
+            }
+        });
     }
     selectColor(event) {
-        this.colorSquareTargets.forEach((element) => {
-            element.classList.remove('selected');
-        });
-        event.currentTarget.classList.add('selected');
-        this.selectTarget.value = event.currentTarget.dataset.colorId;
-        }
+        //this.setSelectedColor(event.currentTarget.dataset.colorId);
+        const clickedColor = event.currentTarget.dataset.colorId;
+
+//        this.colorIdValue = event.currentTarget.dataset.colorId;
+        this.colorIdValue = clickedColor == this.colorIdValue ? null : clickedColor;
+
+    }
+
 }

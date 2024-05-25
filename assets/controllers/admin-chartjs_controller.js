957df68a078241f3c05bd26db5ed78f4d50341a5
BETARI Amine <a.betari@inforca.mc>
2024-05-25 12:09:28 +0200

    Example Chart JS

---
+++ assets/controllers/admin-chartjs_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from 'stimulus';
+
+
+export default class extends Controller {
+    onChartConnect(event) {
+        this.chart = event.detail.chart;
+
+        setTimeout(() => {
+            this.setNewData();
+        }, 5000);
+    }
+
+    setNewData() {
+        this.chart.data.datasets[0].data[2] = 30;
+        this.chart.update();
+    }
+}
